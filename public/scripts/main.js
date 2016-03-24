1ed617f8e6041953bd4598a2b2c8d167b96f4c67
David Pickart <pickartd@carleton.edu>
2016-03-24 11:31:38 -0400

    Interaction pt 1

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,14 +1,47 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-	// Form validation
+	// UI INTERACTION
+
 	$("#generateButton").click(function () {
+
  		var email = $("#emailInput").val();
  		var date = $("#dateInput").val();
+
 	 	if (email.length > 0 && date.length > 0) {
-		 	$("#message").text("Generated");
+		 	flash("Generated");
 	 	} else {
-	 		$("#message").text("Fill both the fields idiot");
+	 		flash("Fill both the fields idiot");
 	 	}
 	});
 
 });
+
+/* UI HELPERS */
+
+function flash(message) {
+	$("#error-flash").text(message);
+}
+
+/* HELPERS */
+
+function post(path, data, callback) {
+	$.ajax({
+		url: path, 
+	 	type: "POST", 
+ 		contentType: "application/json; charset=utf-8",
+        dataType: "json",
+	 	success: callback,
+	 	error: function (e) {
+	 		flash(e.responseText);
+	 	}
+	 });
+}
+
+// http://stackoverflow.com/a/30905277
+function copyToClipboard(text) {
+    var $temp = $("<input>")
+    $("body").append($temp);
+    $temp.val(text).select();
+    document.execCommand("copy");
+    $temp.remove();
+}
