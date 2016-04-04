88af3d255c85f882b8610137c58debe942f19e80
David Pickart <pickartd@carleton.edu>
2016-04-04 14:19:44 -0500

    Add skeleton.css

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -84,7 +84,6 @@ function copyToClipboard(text) {
 }
 
 function validateEmail (email) {
-	console.log(email);
     var re = /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i;
     return re.test(email);
 }
